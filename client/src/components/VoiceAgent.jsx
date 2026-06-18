cb3c8291cf2afc64484e716ef39bf96b9edc3c68
tiya <tiya.french@gmail.com>
2026-06-18 18:36:48 +0300

    commented out the eleven labs chatbot

--- client/src/components/VoiceAgent.jsx
+++ client/src/components/VoiceAgent.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+{ /* import { useEffect } from "react";
 
 export default function VoiceAgent() {
   useEffect(() => {
@@ -23,3 +23,4 @@ export default function VoiceAgent() {
     />
   );
 }
+*/ }
